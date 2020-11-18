b0022b4db4121e1d581e9d3a1fb4ac71bf9693ed
thatchants <andrew@bookfamily.com>
2020-11-18 11:03:58 -0700

    experimenting with wget

--- ss.py
+++ ss.py
@@ -5,14 +5,18 @@ import pickle
 import wget
 
 def connection(c, addr):
+    print('made it to connection')
     #recvd_data = c.recv(1024)
     #data = pickle.loads(recvd_data)
-    data = [2, [('129.82.44.69', '7812'), ('129.82.44.73', '1542')], 'google.com']
-    if data[0] == 0:
-        end_of_chain(data[3])
+    data = [2, [('129.82.44.69', '7812'), ('129.82.44.73', '1542')], 'google.com/index.html']
+    if data[0] == 0 or True:
+        end_of_chain(data[2])
 
 def end_of_chain(url):
-    tmp.txt = wget.download(url)
+    print(getcwd())
+    #print(url)
+    #tmp_file = wget.download(url)
+    #print('end_of_chain')
 
 def main():
     #print hostname and port
@@ -26,10 +30,12 @@ def main():
     
     ss_socket.listen(5)
     
-    while(True):
+    test_bool = True
+    while(test_bool):
         #c, addr = ss_socket.accept()
         c, addr = "test", "this means nothing"
         _thread.start_new_thread(connection, (c, addr, ))
+        test_bool = False
 
 if __name__ == "__main__":
     main()
